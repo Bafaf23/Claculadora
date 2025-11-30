2a79cd354484f126841b3f54c698029a20630085
Bryant Facenda <bryantffacen@gmail.com>
2025-11-29 22:37:16 -0400

    correciones en el calculator-objetc.js y index.html

--- js/calculator-objetc.js
+++ js/calculator-objetc.js
@@ -36,62 +36,65 @@ class Calculadora {
   division() {
     let valor1 = this.valor1;
     let valor2 = this.valor2;
-
-    if (valor2 == 0) {
-      return true;
+    if (valor2 === 0) {
+      return;
+    } else {
+      const resultado = valor1 / valor2;
+      return resultado;
     }
-    const resultado = valor1 / valor2;
-    return resultado;
   }
+}
+
+function mensaje(mensaje) {
+  return (document.getElementById("resultado").textContent = mensaje);
+  /* mostar.innerHTML = `<p id="resultado" class="font-bold text-xl">${mensaje}</p>`;
+  if (alerResultdo) alerResultdo.remove();
+  pantalla.appendChild(mostar); */
 }
 
 function operacion() {
   let input1 = parseFloat(document.getElementById("input1").value);
   let input2 = parseFloat(document.getElementById("input2").value);
   let selec = document.getElementById("select-operacion").value;
-  let pantalla = document.getElementById("pantalla");
 
   let calculo = new Calculadora(input1, input2);
 
+  if (isNaN(input1)) {
+    let entrada = document.getElementById("input1");
+    let error = `Asegurate de ingresar un numero`;
+    entrada.style.borderColor = "red";
+    return mensaje(error);
+  } else {
+    let entrada = document.getElementById("input1");
+    entrada.style.borderColor = "black";
+  }
+
+  if (isNaN(input2)) {
+    let entrada = document.getElementById("input2");
+    let error = `Asegurate de ingresar un numero`;
+    entrada.style.borderColor = "red";
+    return mensaje(error);
+  } else {
+    let entrada = document.getElementById("input2");
+    entrada.style.borderColor = "black";
+  }
+
   if (selec == "+") {
     let resultado = calculo.suma();
-    let mostar = document.createElement("div");
-    let alerResultdo = document.getElementById("resultado");
-    mostar.innerHTML = `<p id="resultado" class="font-bold text-xl">${resultado}</p>`;
-    if (alerResultdo) alerResultdo.remove();
-    pantalla.appendChild(mostar);
-    return resultado;
+    return mensaje(resultado.toFixed(3));
   } else if (selec == "-") {
     let resultado = calculo.resta();
-    let mostar = document.createElement("div");
-    let alerResultdo = document.getElementById("resultado");
-    mostar.innerHTML = `<p id="resultado" class="font-bold text-xl">${resultado}</p>`;
-    if (alerResultdo) alerResultdo.remove();
-    pantalla.appendChild(mostar);
-    return resultado;
+    return mensaje(resultado.toFixed(3));
   } else if (selec == "*") {
     let resultado = calculo.multiplicacion();
-    let mostar = document.createElement("div");
-    let alerResultdo = document.getElementById("resultado");
-    mostar.innerHTML = `<p id="resultado" class="font-bold text-xl">${resultado}</p>`;
-    if (alerResultdo) alerResultdo.remove();
-    pantalla.appendChild(mostar);
-    return resultado;
+    return mensaje(resultado.toFixed(3));
   } else if (selec == "/") {
     let resultado = calculo.division();
-    if (resultado == true) {
-      let alerResultdo = document.getElementById("resultado");
-      let mostar = document.createElement("div");
-      mostar.innerHTML = `<p id="resultado" class="font-bold text-xl">No se puede dividir por 0, animal</p>`;
-      if (alerResultdo) alerResultdo.remove();
-      pantalla.appendChild(mostar);
-      return resultado;
+    if (resultado === undefined) {
+      let error = "No se puede dividir entre 0, animal";
+      return mensaje(error);
     }
-    let mostar = document.createElement("div");
-    let alerResultdo = document.getElementById("resultado");
-    mostar.innerHTML = `<p id="resultado" class="font-bold text-xl">${resultado}</p>`;
-    if (alerResultdo) alerResultdo.remove();
-    pantalla.appendChild(mostar);
+    return mensaje(resultado.toFixed(4));
   }
 }
 
